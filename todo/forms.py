68de36629952fb0f7470f6f3d538e50ae60d987f
Monirul Islam <mr.monirul.dev@gmail.com>
2025-07-14 22:09:37 +0600

    add reuseable template

--- todo/forms.py
+++ todo/forms.py
@@ -10,10 +10,14 @@ class TodoCreateFrom(forms.ModelForm):
             "description",
         ]
         widgets = {
-            "text": forms.TextInput(attrs={"class": "w-full border px-3 py-2 rounded"}),
+            "text": forms.TextInput(
+                attrs={
+                    "class": "w-full border border-gray-200 outline-none px-3 py-2 rounded"
+                }
+            ),
             "description": forms.Textarea(
                 attrs={
-                    "class": "w-full border px-3 py-2 rounded",
+                    "class": "w-full border border-gray-200 px-3 py-2 rounded outline-none",
                     "rows": "2",
                 }
             ),
